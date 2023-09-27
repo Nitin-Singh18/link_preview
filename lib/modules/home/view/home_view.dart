240e54d82dcdca9ef2a4727212026804a8c1bb13
Nitin Singh <i.nitinsingh18@gmail.com>
2023-09-27 15:14:30 +0530

    Fix

--- lib/modules/home/view/home_view.dart
+++ lib/modules/home/view/home_view.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:url_preview_app/common/widget/tile.dart';
-
 import '../../../../common/const/app_colors.dart';
 import '../../../common/widget/dialog.dart';
 import '../../../../common/widget/custom_button.dart';
@@ -32,73 +31,77 @@ class _HomeViewState extends ConsumerState<HomeView> {
     return Scaffold(
       backgroundColor: AppColor.backGroundColor,
       body: urlList.isEmpty
-          ? const Center(
-              child: Text(
-                'No Link Added',
-                style: TextStyle(color: AppColor.mainColor),
+          ? const SafeArea(
+              child: Center(
+                child: Text(
+                  'No Link Added',
+                  style: TextStyle(color: AppColor.mainColor),
+                ),
               ),
             )
-          : Column(
-              children: [
-                SizedBox(
-                  height: 8.h,
-                ),
-                Expanded(
-                  child: ReorderableListView.builder(
-                    itemCount: urlList.length,
-                    proxyDecorator: (child, index, animation) {
-                      return Material(
-                        color: AppColor.backGroundColor,
-                        child: child,
-                      );
-                    },
-                    onReorder: (oldIndex, newIndex) => ref
-                        .read(homeViewModelProvider.notifier)
-                        .reorderItems(oldIndex, newIndex),
-                    itemBuilder: (context, index) {
-                      final url = urlList[index];
-                      return Padding(
-                        key: Key(url.id.toString()),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 10.0.w,
-                        ),
-                        child: Slidable(
-                          key: ValueKey(url),
-                          endActionPane: ActionPane(
-                              motion: const StretchMotion(),
-                              children: [
-                                SlidableAction(
-                                  onPressed: (context) async {
-                                    await showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        return deleteDialog(
-                                          context,
-                                          () {
-                                            ref
-                                                .read(homeViewModelProvider
-                                                    .notifier)
-                                                .deleteUrl(url);
-                                          },
-                                        );
-                                      },
-                                    );
-                                  },
-                                  icon: Icons.delete,
-                                  foregroundColor: AppColor.mainColor,
-                                  backgroundColor: AppColor.backGroundColor,
-                                )
-                              ]),
-                          closeOnScroll: false,
-                          child: Tile(
-                            url: url,
-                          ),
-                        ),
-                      );
-                    },
+          : SafeArea(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 8.h,
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: ReorderableListView.builder(
+                      itemCount: urlList.length,
+                      proxyDecorator: (child, index, animation) {
+                        return Material(
+                          color: AppColor.backGroundColor,
+                          child: child,
+                        );
+                      },
+                      onReorder: (oldIndex, newIndex) => ref
+                          .read(homeViewModelProvider.notifier)
+                          .reorderItems(oldIndex, newIndex),
+                      itemBuilder: (context, index) {
+                        final url = urlList[index];
+                        return Padding(
+                          key: Key(url.id.toString()),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 10.0.w,
+                          ),
+                          child: Slidable(
+                            key: ValueKey(url),
+                            endActionPane: ActionPane(
+                                motion: const StretchMotion(),
+                                children: [
+                                  SlidableAction(
+                                    onPressed: (context) async {
+                                      await showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return deleteDialog(
+                                            context,
+                                            () {
+                                              ref
+                                                  .read(homeViewModelProvider
+                                                      .notifier)
+                                                  .deleteUrl(url);
+                                            },
+                                          );
+                                        },
+                                      );
+                                    },
+                                    icon: Icons.delete,
+                                    foregroundColor: AppColor.mainColor,
+                                    backgroundColor: AppColor.backGroundColor,
+                                  )
+                                ]),
+                            closeOnScroll: false,
+                            child: Tile(
+                              url: url,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
       bottomNavigationBar: Container(
         color: AppColor.backGroundColor,
